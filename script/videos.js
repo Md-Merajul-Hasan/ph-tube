69647a4079ac465cdb737b4397ae71521e4d9ae7
Jim <merajuljim@gmai.com>
2024-10-05 10:56:50 +0600

    added videos adding times to them withif else

--- script/videos.js
+++ script/videos.js
@@ -25,11 +25,39 @@ loadCategories();
 const loadVideos = () => {
     fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
         .then(res => res.json())
-        .then(data => console.log(data.videos))
+        .then(data => displayVideos(data.videos))
         .catch(error => console.log(error));
 };
 // display videos
 const displayVideos = videos => {
-    
-}
+       const videoSection = document.getElementById('videos');
+       videos.forEach(video => {
+        const card = document.createElement("div");
+        card.classList = "card card-compact";
+        card.innerHTML =   
+        `<figure class="h-[200px] relative">
+           <img
+           src=${video.thumbnail}
+           alt="Thumbnail" class="h-full w-full object-cover"/>
+           <span class="text-white bg-black rounded-md px-1 absolute right-2 bottom-2">${video.others.posted_date}</span>
+        </figure>
+        <div class="px-0 py-2 flex gap-2">
+           <div class = "w-[50px] h-[50px]">
+           <img class="h-full w-full rounded-full" src="${video.authors[0].profile_picture}"> 
+           </div>
+           <div>
+           <p class="font-bold text-1xl">${video.title}</p>
+           <div class="flex gap-2 items-center">
+            <p>${video.authors[0].profile_name}</p>
+            ${
+                video.authors[0].verified? '<img class="h-[20px] w-[20px]" src="asset/verified.png">':''
+            }
+           </div>
+           <p>${video.others.views}</p>
+           </div>
+        </div>
+        `;
+        videoSection.append(card);
+       });
+};
 loadVideos();
